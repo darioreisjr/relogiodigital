3065e11f5b545323fd568c611cbac31c548aa2ef
Dario Reis <guttyerresjunior1995@gmail.com>
2023-06-30 00:18:52 -0300

    fix: hours error by adding 0

--- src/App.tsx
+++ src/App.tsx
@@ -30,12 +30,14 @@ export default function App() {
   return (
    <section className='h-screen flex flex-col justify-center items-center bg-gray-950'>
 
-    <h1 className='text-gray-200 p-3 text-4xl'>RELÓGIO DIGITAL</h1>
+    <img className='pb-7 sm:w-1/4 w-72' src="./logo.png" alt="" />
 
     <div className='flex items-center gap-2'>
 
       <div className='flex justify-center items-center rounded-lg'>
-        <h2 className='sm:text-4xl text-2xl text-gray-200 bg-gray-800  sm:p-14 p-11  justify-center items-center'>{hora}</h2>
+        <h2 className='sm:text-4xl text-2xl text-gray-200 bg-gray-800  sm:p-14 p-11  justify-center items-center'>
+        {hora <= 9 ? `0${hora}` : `${hora}` }
+        </h2>
       </div>
 
       <div className='flex justify-center items-center'>
